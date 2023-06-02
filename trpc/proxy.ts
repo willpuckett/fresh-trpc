441c958007c893e41c19ffbbc43d6a9e90eec87e
Will Puckett <willpuckett@gmail.com>
2023-06-01 23:18:41 -0700

    nope

--- trpc/proxy.ts
+++ trpc/proxy.ts
@@ -1,6 +1,9 @@
 import { createTRPCProxyClient, httpBatchLink, loggerLink } from '@trpc/client/'
 import type { AppRouter } from './trpc_router.ts'
 
+const where = window.location
+  ? window.location?.origin
+  : 'https://tropic.deno.dev'
 const url = new URL(window.location?.origin)
 url.pathname = '/api'
 export const proxy = createTRPCProxyClient<AppRouter>({
